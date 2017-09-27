bdb70fda3b0245cd22a69a0686f9684881774b9d
Mateusz Choma <mateusz@amazingdesign.eu>
2017-09-27 18:28:28 +0200

    Cleaning

--- js/package.js
+++ js/package.js
@@ -1,8 +1,8 @@
-function PackageTopRight() {
+function Package() {
     this.handle;
 }
 
-PackageTopRight.prototype.init = function () {
+Package.prototype.init = function () {
     // Dodanie elementu DOM do przestrzeni gry
     // @TODO: Losowanie obiektu
     var packages = ['package', 'package1', 'package2', 'package3'];
@@ -44,96 +44,14 @@ PackageTopRight.prototype.init = function () {
 };
 
 
-PackageTopRight.prototype.moveLeft = function () {
+Package.prototype.moveLeft = function () {
     this.handle.addClass('moveLeft');
 };
 
-PackageTopRight.prototype.moveRight = function () {
+Package.prototype.moveRight = function () {
     this.handle.addClass('moveRight');
 };
 
-PackageTopRight.prototype.moveDown = function () {
+Package.prototype.moveDown = function () {
     this.handle.addClass('moveDown');
 };
-
-// function PackageTopLeft() {
-//     this.handle;
-// }
-
-// PackageTopLeft.prototype.init = function () {
-//     // Dodanie elementu DOM do przestrzeni gry
-//     // @TODO: Losowanie obiektu
-//     var packages1 = ['highLeft'];
-//     // Losujemy liczbe z zakresu kluczy w tablicy obstacles
-//     var random = Math.floor(Math.random() * packages1.length);
-
-//     $('#game').append('<div class="package1 ' + packages1[random] + '"></div>');
-//     this.handle = $('#game .package1:last-child');
-// };
-
-
-// PackageTopLeft.prototype.moveRight = function () {
-//     // Wprawienie przeszkody w ruch
-//     // Dodac klase move na przeszkodzie
-
-//     this.handle.addClass('moveRight');
-// };
-// PackageTopLeft.prototype.moveDown = function () {
-
-//     this.handle.addClass('moveDown');
-// };
-
-
-// function PackageBottomLeft() {
-//     this.handle;
-// }
-
-// PackageBottomLeft.prototype.init = function () {
-//     // Dodanie elementu DOM do przestrzeni gry
-//     // @TODO: Losowanie obiektu
-//     var packages2 = ['BottomLeft'];
-//     // Losujemy liczbe z zakresu kluczy w tablicy obstacles
-//     var random = Math.floor(Math.random() * packages2.length);
-
-//     $('#game').append('<div class="package2 ' + packages2[random] + '"></div>');
-//     this.handle = $('#game .package2:last-child');
-// };
-
-
-// PackageBottomLeft.prototype.moveLeft = function () {
-//     // Wprawienie przeszkody w ruch
-//     // Dodac klase move na przeszkodzie
-
-//     this.handle.addClass('moveLeft');
-// };
-// PackageBottomLeft.prototype.moveDown = function () {
-
-//     this.handle.addClass('moveDown');
-// };
-
-// function PackageBottomRight() {
-//     this.handle;
-// }
-
-// PackageBottomRight.prototype.init = function () {
-//     // Dodanie elementu DOM do przestrzeni gry
-//     // @TODO: Losowanie obiektu
-//     var packages3 = ['BottomRight'];
-//     // Losujemy liczbe z zakresu kluczy w tablicy obstacles
-//     var random = Math.floor(Math.random() * packages3.length);
-
-//     $('#game').append('<div class="package3 ' + packages3[random] + '"></div>');
-//     this.handle = $('#game .package3:last-child');
-// };
-
-
-// PackageBottomRight.prototype.moveLeft = function () {
-//     // Wprawienie przeszkody w ruch
-//     // Dodac klase move na przeszkodzie
-
-//     this.handle.addClass('moveLeft');
-// };
-// PackageBottomRight.prototype.moveDown = function () {
-
-//     this.handle.addClass('moveDown');
-// };
